85d6f414d5c0ab0b7d61cca086b47637e7cd5800
AngelPerez10 <111476102+AngelPerez10@users.noreply.github.com>
2022-10-12 11:33:20 -0500

    Update examen.c

--- Estrutura de datos/examen.c
+++ Estrutura de datos/examen.c
@@ -1,21 +1,24 @@
 #include<stdio.h>
 
-void result(int,int);
-int main(){
-	
-	int num,b;
-	printf("INGRESA EL NUMERO DE LA SECUENCIA \n");
-	scanf("%d",&num);
-	
-	result(num, b);
-	return 0;
-	
-}
-void result(int a, int b){
-	if(a < b){
-		printf("\n%d ", a);
-		a = a * 3;
-		result(a,b);
+int problema2(int a){
+	if(a == 0){
+		return 0;
+	}else if(a == 1){
+		return 5;
+	}else{
+		return (problema2(a-1) * 3);
 	}
+}
 
+int main(){
+	int dominio = 0;
+
+	printf("INGRESA HASTA QUE RANGO DESEAS LA SUCESIÓ");
+	scanf("%d", &dominio);
+
+	for(int i = 1; i <= dominio; i++){
+		printf("Dominio = %d\n", i);
+		printf("Rango = %d\n", problema2(i));
+	}
+	printf("\n");
 }
