ce051273e1c45c1af18fb6788f75b88376f76c6e
AngelPerez10 <111476102+AngelPerez10@users.noreply.github.com>
2022-10-12 11:47:46 -0500

    Update examen.c

--- Estrutura de datos/examen.c
+++ Estrutura de datos/examen.c
@@ -1,25 +1,26 @@
 //Adolfo Angel Perez Salado
+
 #include<stdio.h>
 
-int problema2(int a){
+int recusivo(int a){
 	if(a == 0){
 		return 0;
 	}else if(a == 1){
 		return 5;
 	}else{
-		return (problema2(a-1) * 3);
+		return (recusivo(a-1) * 3);
 	}
 }
 
 int main(){
-	int dominio = 0;
+	int dom = 0;
 
 	printf("INGRESA HASTA QUE RANGO DESEAS LA SUCESIÓ: ");
-	scanf("%d", &dominio);
+	scanf("%d", &dom);
 
-	for(int i = 1; i <= dominio; i++){
-		printf("Dominio = %d\n", i);
-		printf("Rango = %d\n", problema2(i));
+	for(int i = 1; i <= dom; i++){
+		printf("El dominio = %d\n", i);
+		printf("El rango es igual a = %d\n", recusivo(i));
 	}
 	printf("\n");
 }
